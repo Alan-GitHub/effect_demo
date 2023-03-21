f67a1c4232c7fd3c587cf3544b50ef340c4b6f00
alan <584840653@qq.com>
2023-03-21 16:08:23 +0800

    限制小圆在底圆边界内

--- lib/src/animation_use/joystick_component.dart
+++ lib/src/animation_use/joystick_component.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
@@ -18,29 +20,29 @@ class JoystickComponent extends CustomPainter {
   final double littleCircleR;
   final ValueNotifier<JoystickData> joystickData;
 
+  late double bigCircleOriginDx;
+  late double bigCircleOriginDy;
+  late double littleCircleOriginDx;
+  late double littleCircleOriginDy;
+
   @override
   void paint(Canvas canvas, Size size) {
-    // double bgR = 50;
+    Paint paint = Paint();
 
-    // 底圆
-    // canvas.drawCircle(
-    //     const Offset(0, 0),
-    //     bgR,
-    //     paint
-    //       ..style = PaintingStyle.fill
-    //       ..color = Colors.blue.withOpacity(0.2));
+    if (joystickData.value.panDetail is DragDownDetails) {
+      paintDown(canvas, size, paint);
+    } else if (joystickData.value.panDetail is DragEndDetails) {
+      paintReset(canvas, paint);
+    } else if (joystickData.value.panDetail is DragUpdateDetails) {
+      paintUpdate(canvas, paint);
+    }
+  }
 
-    // 手势小圆
-    // canvas.drawCircle(
-    //     const Offset(0, 0),
-    //     bgR / 2,
-    //     paint
-    //       ..style = PaintingStyle.fill
-    //       ..color = Colors.blue.withOpacity(0.9));
-
+  // 绘制手指按下时图形
+  void paintDown(Canvas canvas, Size size, Paint paint) {
     //大圆原点坐标
-    double bigCircleOriginDx = joystickData.value.offset.dx - bigCircleR;
-    double bigCircleOriginDy = joystickData.value.offset.dy - bigCircleR;
+    bigCircleOriginDx = joystickData.value.offset.dx - bigCircleR;
+    bigCircleOriginDy = joystickData.value.offset.dy - bigCircleR;
 
     //大圆边界处理
     if (bigCircleOriginDx < 0) {
@@ -60,10 +62,68 @@ class JoystickComponent extends CustomPainter {
     }
 
     //小圆原点坐标
-    double littleCircleOriginDx = bigCircleOriginDx + (bigCircleR - littleCircleR);
-    double littleCircleOriginDy = bigCircleOriginDy + (bigCircleR - littleCircleR);
+    littleCircleOriginDx = bigCircleOriginDx + (bigCircleR - littleCircleR);
+    littleCircleOriginDy = bigCircleOriginDy + (bigCircleR - littleCircleR);
 
-    Paint paint = Paint();
+    if (bigCircleImage != null) {
+      canvas.drawImage(bigCircleImage!, Offset(bigCircleOriginDx, bigCircleOriginDy), paint);
+    }
+
+    if (littleCircleImage != null) {
+      canvas.drawImage(littleCircleImage!, Offset(littleCircleOriginDx, littleCircleOriginDy), paint);
+    }
+  }
+
+  void paintUpdate(Canvas canvas, Paint paint) {
+    if (bigCircleImage != null) {
+      canvas.drawImage(bigCircleImage!, Offset(bigCircleOriginDx, bigCircleOriginDy), paint);
+    }
+
+    DragUpdateDetails update = joystickData.value.panDetail as DragUpdateDetails;
+
+    littleCircleOriginDx += update.delta.dx;
+    littleCircleOriginDy += update.delta.dy;
+
+    // 小圆心坐标
+    double littleCircleCenterDx = littleCircleOriginDx + littleCircleR;
+    double littleCircleCenterDy = littleCircleOriginDy + littleCircleR;
+
+    // 大圆圆心
+    double bigCircleCenterDx = bigCircleOriginDx + bigCircleR;
+    double bigCircleCenterDy = bigCircleOriginDy + bigCircleR;
+
+    // 两个圆心相对坐标，以底圆心为原点
+    double rx = littleCircleCenterDx - bigCircleCenterDx;
+    double ry = -(littleCircleCenterDy - bigCircleCenterDy);
+
+    // 手指接触点离底圆圆心的距离
+    double distance = sqrt(pow(rx, 2) + pow(ry, 2));
+    // 角度
+    double thta = atan2(ry, rx);
+
+
+    if (distance > bigCircleR) {
+      littleCircleCenterDx = bigCircleR * cos(thta) + bigCircleCenterDx;
+      littleCircleCenterDy = bigCircleR * sin(thta) + bigCircleCenterDy;
+    }
+
+    // 小圆左上角在坐标系中的坐标
+    littleCircleOriginDx = littleCircleCenterDx - littleCircleR;
+    littleCircleOriginDy = littleCircleCenterDy - littleCircleR;
+
+    if (littleCircleImage != null) {
+      canvas.drawImage(littleCircleImage!, Offset(littleCircleOriginDx, littleCircleOriginDy), paint);
+    }
+  }
+
+  void paintReset(Canvas canvas, Paint paint) {
+    //大圆原点坐标
+    bigCircleOriginDx = joystickData.value.offset.dx - bigCircleR;
+    bigCircleOriginDy = joystickData.value.offset.dy - bigCircleR;
+
+    //小圆原点坐标
+    littleCircleOriginDx = bigCircleOriginDx + (bigCircleR - littleCircleR);
+    littleCircleOriginDy = bigCircleOriginDy + (bigCircleR - littleCircleR);
 
     if (bigCircleImage != null) {
       canvas.drawImage(bigCircleImage!, Offset(bigCircleOriginDx, bigCircleOriginDy), paint);
