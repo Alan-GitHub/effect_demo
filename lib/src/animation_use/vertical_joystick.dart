03512f4c5086915dbb2dc161dad80bef27ca7c94
alan <584840653@qq.com>
2023-03-25 15:18:18 +0800

    垂直操纵杆小三角变量时，背景同时应该有发光效果

--- lib/src/animation_use/vertical_joystick.dart
+++ lib/src/animation_use/vertical_joystick.dart
@@ -181,6 +181,10 @@ class VerticalJoyStickPainter extends CustomPainter {
     path1.lineTo(rx, ry - 11 * scale);
     path1.lineTo(rx + 6 * scale, ry - 3 * scale);
     path1.close();
+
+    if (dy < 0) {
+      drawShadows(canvas, path1, [const BoxShadow(color: RCColors.blue_2FD9F8, blurRadius: 10, spreadRadius: 12)], -1);
+    }
     canvas.drawPath(path1, _paint);
 
     /// 小球球里面的三角形（下）
@@ -194,11 +198,42 @@ class VerticalJoyStickPainter extends CustomPainter {
     path2.lineTo(rx, ry + 11 * scale);
     path2.lineTo(rx + 6 * scale, ry + 3 * scale);
     path2.close();
+
+    if (dy > 0) {
+      drawShadows(canvas, path2, [const BoxShadow(color: RCColors.blue_2FD9F8, blurRadius: 10, spreadRadius: 12)], 1);
+    }
     canvas.drawPath(path2, _paint);
   }
 
   @override
   bool shouldRepaint(covariant VerticalJoyStickPainter oldDelegate) {
     return oldDelegate.offset != offset;
+  }
+
+  /// 绘制阴影封装函数
+  void drawShadows(Canvas canvas, Path path, List<BoxShadow> shadows, double revise) {
+    for (final BoxShadow shadow in shadows) {
+      final Paint shadowPainter = shadow.toPaint();
+      if (shadow.spreadRadius == 0) {
+        canvas.drawPath(path.shift(shadow.offset), shadowPainter);
+      } else {
+        /// 生成一个刚好包住目标path图案的矩形
+        Rect zone = path.getBounds();
+
+        /// 阴影传播半径 + 矩形宽高得到缩放比例
+        double xScale = (zone.width + shadow.spreadRadius) / zone.width;
+        double yScale = (zone.height + shadow.spreadRadius) / zone.height;
+
+        /// 获取单位4x4矩阵
+        Matrix4 m4 = Matrix4.identity();
+
+        /// 缩放指定比例。不只图形会变大，矩形中心坐标也会移动对应缩放倍数
+        m4.scale(xScale, yScale);
+
+        /// 缩放时，坐标也移动了缩放倍数，需要平移回原始位置
+        m4.translate(-zone.center.dx * (xScale - 1.0) / xScale, -zone.center.dy * (yScale - 1.0) / yScale + revise);
+        canvas.drawPath(path.shift(shadow.offset).transform(m4.storage), shadowPainter);
+      }
+    }
   }
 }
